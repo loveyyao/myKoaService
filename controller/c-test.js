6852f9b3bc0f2d1b2d758ed0953c2ec9140d69c6
yaobisi <320130049@qq.com>
2021-08-09 14:24:55 +0800

    test 接口增删改查

--- controller/c-test.js
+++ controller/c-test.js
@@ -98,7 +98,129 @@ const addTest = async ctx => {
   })
 }
 
+// 删除
+const delTest = async ctx => {
+  const query = ctx.request.query
+  if (!query.id) {
+    ctx.body = getResponse.error(null, 500, 'id不能为空')
+    return
+  }
+  let test = null
+  try {
+    test = await Test.findOne({
+      where: {
+        del_flag: {
+          [Op.eq]: '0'
+        },
+        id: {
+          [Op.eq]: query.id
+        }
+      }
+    })
+  } catch (e) {
+    test = null
+  }
+  if (!test) {
+    ctx.body = getResponse.error(null, 500, 'id不存在')
+    return
+  }
+  await Test.update({
+    del_flag: '1'
+  }, {
+    where: {
+      id: query.id
+    }
+  }).then(res => {
+    ctx.body = getResponse.success(res)
+  }).catch(err => {
+    ctx.body = getResponse.error(err, 500, '删除失败')
+  })
+}
+
+// 获取
+const getTestById = async ctx => {
+  const query = ctx.request.query
+  if (!query.id) {
+    ctx.body = getResponse.error(null, 500, 'id不能为空')
+    return
+  }
+  await Test.findOne({
+    where: {
+      del_flag: {
+        [Op.eq]: '0'
+      },
+      id: {
+        [Op.eq]: query.id
+      }
+    }
+  }).then(res => {
+    ctx.body = getResponse.success(res)
+  }).catch(err => {
+    ctx.body = getResponse.error(err)
+  })
+}
+
+const updateTest = async ctx => {
+  const data = ctx.request.body
+  const token = ctx.headers.authorization
+  // 校验参数
+  const valid = validator(data, testRules.edit)
+  if (valid.length) {
+    ctx.body = getResponse.error(null, 500, valid[0].message)
+    return
+  }
+  // 根据token获取用户信息{ name: username, id: id }
+  let userInfo = null
+  try {
+    userInfo = await verify(token)
+  } catch (e) {
+    userInfo = null
+    ctx.body = getResponse.error(e)
+  }
+  if (!userInfo) return
+  let test = null
+  try {
+    test = await Test.findOne({
+      where: {
+        del_flag: {
+          [Op.eq]: '0'
+        },
+        id: {
+          [Op.eq]: data.id
+        }
+      }
+    })
+  } catch (e) {
+    test = null
+  }
+  if (!test) {
+    // 编辑的时候根据ID没有找到
+    ctx.body = getResponse.error(null, 500, 'id不存在')
+    return
+  }
+  await Test.update({
+    name: data.name,
+    code: data.code,
+    alias: data.alias,
+    update_by: userInfo.name,
+    update_time: utils.time()
+  }, {
+    where: {
+      id: {
+        [Op.eq]: data.id
+      }
+    }
+  }).then(() => {
+    ctx.body = getResponse.success(data.id)
+  }).catch(err => {
+    ctx.body = getResponse.error(err)
+  })
+}
+
 module.exports = {
   ['GET test/list']: selectTestPageList,
-  ['POST test/add']: addTest
+  ['POST test/add']: addTest,
+  ['GET test/delete']: delTest,
+  ['GET test/get']: getTestById,
+  ['POST test/edit']: updateTest
 }
